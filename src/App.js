20b4a8556cc35eadaf433c2f08a5d488fc8f135d
zxfjd3g <258147149@qq.com>
2019-06-14 20:12:51 +0800

    note

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {HashRouter, BrowserRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Login from './pages/login/login.jsx'
 import Admin from './pages/admin/admin.jsx'
 /* 
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -52,6 +52,7 @@ export default class Login extends React.Component {
               </Button>
             </Form.Item>
           </Form>
+          
         </section>
       </div>
     )
