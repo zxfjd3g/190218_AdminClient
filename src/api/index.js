47b9d21ffa927e04f5f682da33a055cf3ef36a04
zxfjd3g <258147149@qq.com>
2019-06-18 11:03:55 +0800

    logout

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,9 @@
 调用自定义ajax请求函数发请求
 每个函数的返回值都是promise对象
 */
-import ajax from "./ajax";
+import jsonp from 'jsonp'
+import {message} from 'antd'
+import ajax from "./ajax"
 // const BASE = 'http://localhost:3001'
 const BASE = ''
 
@@ -21,8 +23,38 @@ export const reqAddUser = (user) => ajax(BASE + '/manage/user/add', user, 'POST'
 
 
 // 简单测试一下
-reqLogin('admin', 'admin').then(result => {
+/* reqLogin('admin', 'admin').then(result => {
   console.log('result', result)
-})
+}) */
+
+/* 
+发jsonp请求获取当前天气信息
+*/
+export const reqWeather = (location) => {
+  const url = `http://api.map.baidu.com/telematics/v3/weather?location=${location}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`
+
+  return new Promise((resolve, reject) => {
+    // 执行请求
+    setTimeout(() => {
+      jsonp(url, {}, (error, data) => {
+        if (!error && data.status === 'success') {
+          const {
+            dayPictureUrl,
+            weather
+          } = data.results[0].weather_data[0]
+          // 成功了, 调用reolve()指定成功的值
+          resolve({
+            dayPictureUrl,
+            weather
+          })
+        } else {
+          message.error('获取天气信息失败!')
+        }
+      })
+    }, 2000)
+  })
+  
+}
+
 
 
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import {withRouter} from 'react-router-dom'
+import { Modal } from 'antd'
 
+import {reqWeather} from '../../api'
+import {removeUser} from "../../utils/storageUtils"
 import memoryUtils from "../../utils/memoryUtils"
 import {formateDate} from '../../utils/dateUtils'
 import menuList from '../../config/menuConfig'
@@ -11,8 +14,11 @@ Admin头部界面
 */
 class Header extends Component {
 
+  static propTypes = { // 给Header(类对象)添加的属性
+
+  }
   // 初始化状态
-  state = {
+  state = { // 给Header的实例(组件对象)添加属性
     currentTime: formateDate(Date.now()), // 当前时间字符串
     dayPictureUrl: '', // 天气图片的url
     weather: '', // 天气的文本
@@ -53,6 +59,40 @@ class Header extends Component {
   }
 
   /* 
+  获取天气信息显示
+  */
+  getWeather = async () => {
+    const { dayPictureUrl, weather} = await reqWeather('北京')
+    // 更新状态
+    this.setState({
+       dayPictureUrl, 
+       weather 
+    })
+  }
+
+  /* 
+  退出登陆
+  */
+  logout = () => {
+
+    // 显示确认框, 点击确定后再退出
+    Modal.confirm({
+      title: '确定退出吗?',
+      onOk: () => {
+        console.log('OK')
+        // 清除保存user数据
+        removeUser()  // local中的
+        memoryUtils.user = {} // 内存中的
+        // 跳转到登陆
+        this.props.history.replace('/login')
+      },
+      onCancel() {
+        console.log('Cancel')
+      },
+    })
+  }
+
+  /* 
   组件即将销毁之前
   做一些收尾的工作: 清除定时器
   */
@@ -63,6 +103,8 @@ class Header extends Component {
   componentDidMount () {
     // 每隔1s更新时间的显示
     this.showCurrentTime()
+    // 获取天气信息显示
+    this.getWeather()
   }
 
   render() {
@@ -79,13 +121,13 @@ class Header extends Component {
       <div className="header">
         <div className="header-top">
           <span>欢迎, {user.username}</span>
-          <a href="javascript:">登出</a>
+          <a href="javascript:" onClick={this.logout}>登出</a>
         </div>
         <div className="header-bottom">
           <div className="header-bottom-left">{title}</div>
           <div className="header-bottom-right">
             <span>{currentTime}</span>
-            <img src={dayPictureUrl} alt="weather"/>
+            {!!dayPictureUrl && <img src={dayPictureUrl} alt="weather" />}
             <span>{weather}</span>
           </div>
         </div>
